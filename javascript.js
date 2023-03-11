6d9d95cdbe6eb3984a63fc291fa6876112f02492
Gethlan2048 <geth2048@gmail.com>
2023-03-11 14:56:08 -0600

    Create js file and add comments

---
+++ javascript.js
@@ -0,0 +1,24 @@
+//declare userScore, computerScore
+//get and return the computers choice of rock,paper,scissors
+
+//get the users choice
+//compare the computer/user choice
+//play until computer or user have 5 points
+//if user chooses rock
+    //if computer scissors then comp losses 
+        //increment player score
+    //if computer paper then comp wins
+        //increment computer score
+    //if comp rock then tie
+//if user chooses paper then
+    //if computer scissors then comp wins
+        //increment computer score
+    //if computer rock then comp looses 
+        //increment player score
+    //if comp paper then tie
+//if user chooses scissors then
+    //if computer rock then comp wins
+        //increment computer score
+    //if computer paper then comp looses 
+        //increment player score
+    //if comp paper then tie
